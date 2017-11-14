3b1d069274d4f72ea40bf221f1058d93f828942f
rickvancollenburg <rickvancollenburg@gmail.com>
2017-11-14 12:52:59 +0100

    test

--- propr-app/app/src/main/java/giphouse/nl/proprapp/ui/group/GroupListActivity.java
+++ propr-app/app/src/main/java/giphouse/nl/proprapp/ui/group/GroupListActivity.java
@@ -136,13 +136,8 @@ public class GroupListActivity extends ListActivity
 		// Handle navigation view item clicks here.
 		final int id = item.getItemId();
 
-		if (id == R.id.nav_camera) {
+		if (id == R.id.nav_manage) {
 			// Handle the camera action
-		} else if (id == R.id.nav_gallery) {
-
-		} else if (id == R.id.nav_slideshow) {
-
-		} else if (id == R.id.nav_manage) {
 
 		} else if (id == R.id.nav_share) {
 
